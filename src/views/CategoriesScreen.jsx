51d411de3ae70c2a980114b956e8e0e2e11994d7
Vane32 <vanessa.bernal.avam@posbrava.io>
2024-09-13 14:53:25 -0500

    Se crean vista basicas

---
+++ src/views/CategoriesScreen.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import { Text } from 'react-native'
+
+const CategoriesScreen = () => {
+  return (
+    <Text>CategoriesScreen</Text>
+  )
+}
+
+export default CategoriesScreen
